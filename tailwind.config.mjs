9b99d2282106818e99b1bad084806d808fe68357
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-01-02 21:42:32 +0800

    header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,26 @@
 const plugin = require("tailwindcss/plugin");
-
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, addComponents }) {
+      addUtilities({
+        ".text-gradient-purple": {
+          color: "transparent",
+          backgroundClip: "text",
+          background: "linear-gradient(to right, #d8b4fe, #f9a8d4)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+        },
+      });
+    }),
+  ],
 };
