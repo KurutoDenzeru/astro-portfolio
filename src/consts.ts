106e8d40a1c071e2c875e7a9d2d78d4470f2a799
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-01-02 21:26:54 +0800

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Portfolio - Kurt Calacday";
+export const SITE_DESCRIPTION =
+	"Interactive online portfolio showcasing comprehensive information about myself, encompassing a diverse array of my work, the various projects I have successfully created, and a detailed overview of my extensive skills and rich professional experiences.";
