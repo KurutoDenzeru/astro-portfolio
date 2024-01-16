e93dae52d520ff32cd38203e450dc0d43032d19f
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-01-17 01:45:13 +0800

    root

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+
+  site: "https://example.com",
   integrations: [tailwind(), sitemap()],
 
   image: {
