5f409d012fd9feedde3c2c99fbdc6ce229b53e60
kurutodenzeru <kurtdenzel51@gmail.com>
2024-09-09 15:24:16 +0800

    build: latest structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kurtcalacday.vercel.app/",
+  integrations: [mdx(), sitemap(), solidJs(), tailwind()],
 
-  site: "https://example.com",
-  integrations: [tailwind(), sitemap(), astroImageTools],
-
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
 });
