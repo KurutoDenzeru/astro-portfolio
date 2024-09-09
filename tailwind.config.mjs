9db0e633ad6eaee985d432e087ac5f04aca8acdf
kurutodenzeru <kurtdenzel51@gmail.com>
2024-09-09 15:43:11 +0800

    build: refactored imports

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,69 @@
-const plugin = require("tailwindcss/plugin");
+import defaultTheme from "tailwindcss/defaultTheme"
+import plugin from 'tailwindcss/plugin';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        "sans": ["Atkinson", ...defaultTheme.fontFamily.sans],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "full",
+          },
+        },
+      },
+      rotate: {
+        "45": "45deg",
+        "135": "135deg",
+        "225": "225deg",
+        "315": "315deg",
+      },
+      animation: {
+        twinkle: "twinkle 2s ease-in-out forwards",
+        meteor: "meteor 3s ease-in-out forwards",
+      },
+      keyframes: {
+        twinkle: {
+          "0%": {
+            opacity: 0,
+            transform: "rotate(0deg)"
+          },
+          "50%": {
+            opacity: 1,
+            transform: "rotate(180deg)"
+          },
+          "100%": {
+            opacity: 0,
+            transform: "rotate(360deg)"
+          },
+        },
+        meteor: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(200%)"
+          },
+          "50%": {
+            opacity: 1
+          },
+          "100%": {
+            opacity: 0,
+            transform: "translateY(0)"
+          },
+        },
+      },
     },
   },
   plugins: [
-    plugin(function ({ addUtilities, addComponents }) {
+    require("@tailwindcss/typography"),
+    plugin(function ({ addUtilities }) {
       addUtilities({
         ".text-gradient-purple": {
           color: "transparent",
