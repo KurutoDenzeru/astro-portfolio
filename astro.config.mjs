9fabbd0d256b816a741cf8b37231c0019900c7c0
kurutodenzeru <kurtdenzel51@gmail.com>
2024-09-11 10:30:49 +0800

    fix: remove analytics due to markdown titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kurtcalacday.vercel.app/",
   integrations: [mdx(), sitemap(), solidJs(), tailwind()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
 });
