4d74502b114296e013100635f13fff2bb02c20c7
kurutodenzeru <kurtdenzel51@gmail.com>
2024-11-07 14:36:21 +0800

    build: remove polyfill import and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import { polyfill } from '@astrojs/webapi'
-
-polyfill(globalThis)
 
 // https://astro.build/config
 export default defineConfig({
