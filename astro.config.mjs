13e7422b9f9e293223ea813cf6820ba0b6becdeb
kurutodenzeru <kurtdenzel51@gmail.com>
2024-11-07 11:10:46 +0800

    build: removed core-js in replacement to @astrojs/lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import lit from '@astrojs/lit';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kurtcalacday.vercel.app/",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind()],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind(), lit()],
 });
