23341bdca85a7a0bd2879fdb8d1c36669fce7810
kurutodenzeru <kurtdenzel51@gmail.com>
2024-11-07 11:45:39 +0800

    build: testing webapi for polyfill

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import lit from '@astrojs/lit';
+import { polyfill } from '@astrojs/webapi'
+
+polyfill(globalThis)
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kurtcalacday.vercel.app/",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind(), lit()],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind()],
 });
