be9f796c66b7133c11f22d57c7c0a13cf884dec2
kurutodenzeru <kurtdenzel51@gmail.com>
2024-11-08 10:29:20 +0800

    build: Update dependencies and remove unnecessary polyfills

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 // https://astro.build/config
 export default defineConfig({
