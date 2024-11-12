32280218982cb14afb19f5c1f639d0ff2fdc381f
kurutodenzeru <kurtdenzel51@gmail.com>
2024-11-12 11:05:54 +0800

    feat: Update Astro configuration for Vercel deployment and enable static prerendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kurtcalacday.vercel.app/",
 	integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 	// output: 'hybrid'
+	output: 'server',
+	adapter: vercel({
+		imageService: true,
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
