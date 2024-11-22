b19a4b8cd9e0d00d89b4ea46999d7db00f91a167
kurutodenzeru <kurtdenzel51@gmail.com>
2024-11-22 17:36:38 +0800

    feat: Update Astro configuration to use Deno adapter and remove static route entries

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,15 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import vercel from '@astrojs/vercel/serverless';
+import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kurtcalacday.vercel.app/",
 	integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 	output: 'hybrid',
-	trailingSlash: 'never',
-	exposeAssets: true,
-	entries: [
-		'/',
-		'/blog/*',
-		'/projects/*',
-		'/legal/*',
-		// Add other static routes here
-	],
 	// output: 'server',
-	adapter: vercel({
+	adapter: deno({
 		imageService: true,
 		webAnalytics: {
 			enabled: true,
