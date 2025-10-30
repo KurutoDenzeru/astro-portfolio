51e0cc7b2dc408978438c3c5e808991effc6a614
KurutoDenzeru <kurtdenzel51@gmail.com>
2025-10-30 04:53:37 -0700

    refactor: simplify RSS feed generation by removing blog posts and adjusting links

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,10 +7,9 @@ type Context = {
 };
 
 export async function GET(context: Context) {
-	const posts = await getCollection("blog");
 	const projects = await getCollection("projects");
 
-	const items = [...posts, ...projects];
+	const items = projects;
 
 	items.sort(
 		(a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
@@ -24,9 +23,7 @@ export async function GET(context: Context) {
 			title: item.data.title,
 			description: item.data.summary,
 			pubDate: item.data.date,
-			link: item.slug.startsWith("blog")
-				? `/blog/${item.slug}/`
-				: `/projects/${item.slug}/`,
+			link: `/projects/${item.slug}/`,
 		})),
 	});
 }
